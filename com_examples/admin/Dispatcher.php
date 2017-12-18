04882c0315ab571c83050eb5a04733dda0f5f009
Alexandr Kosarev <alex7r@ya.ru>
2017-12-18 18:48:53 +0300

    add root files
    files are must to trigger component

---
+++ com_examples/admin/Dispatcher.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 2017. Alexandr Kosarev, @kosarev.by
+ */
+
+namespace Joomplace\Component\Xexamples\Admin;
+
+class Dispatcher extends \Joomplace\X\Dispatcher
+{
+    public static $namespace = 'Joomplace\\Component\\Xexamples';
+}
---
+++ com_examples/admin/xexamples.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Copyright (c) 2017. Alexandr Kosarev, @kosarev.by
+ */
+
+namespace Joomplace\Component\Xexamples\Admin;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Session\Session;
+
+Factory::getDocument()->setMetaData('api-token',Session::getFormToken());
+
+Dispatcher::migrate(__DIR__.DIRECTORY_SEPARATOR.'migrations');
+Dispatcher::getInstance('Example')->execute(Factory::getApplication()->input->get('task','index'));
