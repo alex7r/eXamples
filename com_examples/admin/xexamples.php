7753eedbe0b125937632f16e3312d5e175ee01f3
Alexandr Kosarev <alex7r@ya.ru>
2017-12-19 11:32:04 +0300

    make simple 'show' layout
    remove default task (restful trait will handle it now)
    create 'show' layout
    // now just append "&id=x" to your url to see layout show

--- com_examples/admin/xexamples.php
+++ com_examples/admin/xexamples.php
@@ -11,4 +11,4 @@ use Joomla\CMS\Session\Session;
 Factory::getDocument()->setMetaData('api-token',Session::getFormToken());
 
 Dispatcher::migrate(__DIR__.DIRECTORY_SEPARATOR.'migrations');
-Dispatcher::getInstance('Example')->execute(Factory::getApplication()->input->get('task','index'));
+Dispatcher::getInstance('Example')->execute(Factory::getApplication()->input->get('task'));
